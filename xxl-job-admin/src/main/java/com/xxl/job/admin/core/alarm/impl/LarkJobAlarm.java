3d349e10fff4ec84fc7d8845d9e2d19b663f2b7a
zqx <zhaoqx@hcfunds.com>
2023-03-14 10:11:30 +0800

    add 异常信息推送

--- xxl-job-admin/src/main/java/com/xxl/job/admin/core/alarm/impl/LarkJobAlarm.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/core/alarm/impl/LarkJobAlarm.java
@@ -1,11 +1,14 @@
 package com.xxl.job.admin.core.alarm.impl;
 
 import cn.hutool.core.util.StrUtil;
+import com.xxl.job.admin.controller.JobLogController;
 import com.xxl.job.admin.core.alarm.JobAlarm;
 import com.xxl.job.admin.core.conf.XxlJobAdminConfig;
 import com.xxl.job.admin.core.model.XxlJobInfo;
 import com.xxl.job.admin.core.model.XxlJobLog;
 import com.xxl.job.admin.core.util.LarkUtil;
+import com.xxl.job.core.biz.model.LogResult;
+import com.xxl.job.core.biz.model.ReturnT;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
@@ -16,19 +19,18 @@ public class LarkJobAlarm implements JobAlarm {
 
     @Resource
     LarkUtil larkUtil;
+    @Resource
+    JobLogController jobLogController;
 
     /**
      * lark msg
-     *
-     * @param info
-     * @param jobLog
-     * @return
      */
     @Override
     public boolean doAlarm(XxlJobInfo info, XxlJobLog jobLog) {
         String larkBotUrl = XxlJobAdminConfig.getAdminConfig().getLarkBotUrl();
+        ReturnT<LogResult> res = jobLogController.logDetailCat(jobLog.getExecutorAddress(), jobLog.getTriggerTime().getTime(), jobLog.getId(), 1);
         if (StringUtils.hasLength(larkBotUrl)) {
-            larkUtil.send(larkBotUrl, StrUtil.format("执行器[{}] 任务[{}] \n {}", jobLog.getTitle(), info.getJobDesc(), StrUtil.isBlank(jobLog.getHandleMsg()) ? "" : jobLog.getHandleMsg()));
+            larkUtil.send(larkBotUrl, StrUtil.format("执行器[{}] 任务[{}] \n {}", jobLog.getTitle(), info.getJobDesc(), res.getContent().getLogContent()));
         }
         return false;
     }
